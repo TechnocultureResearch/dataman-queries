5fb1e449918d8636cee0595f47b5828c5778a453
SintuTCR <97965156+SintuTCR@users.noreply.github.com>
2022-11-07 19:05:22 +0530

    Add query to fetch customer details from V_CustomerMaster table

--- queries.sql
+++ queries.sql
@@ -2,3 +2,6 @@ SELECT pm.Code as PhaseCode, sm.Name as SiteName, pm.Name as PhaseName, pm.U_Ent
 FROM Technoculture.dbo.SiteMast as sm
 RIGHT JOIN Technoculture.dbo.PhaseMast as pm
 ON sm.Code = pm.Site_Code
+
+SELECT SubCode, Manual_Code, Account_Name, Display_Name, Initial, So_Wo_Do, City, PIN, Phone, Mobile, E_Mail, Ac_Group, PAN
+FROM Technoculture.dbo.V_CustomerMaster
