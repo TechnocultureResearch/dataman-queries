a7bdbee784718ae912255a4570555e2081f0cbce
SintuTCR <97965156+SintuTCR@users.noreply.github.com>
2022-11-08 18:42:58 +0530

    Add query to fetch sale details

--- queries.sql
+++ queries.sql
@@ -5,3 +5,8 @@ ON sm.Code = pm.Site_Code
 
 SELECT SubCode, Manual_Code, Account_Name, Display_Name, Initial, So_Wo_Do, City, PIN, Phone, Mobile, E_Mail, Ac_Group, PAN
 FROM Technoculture.dbo.V_CustomerMaster
+
+SELECT  us.DocId, us.V_Type, us.V_Prefix, us.V_Date, us.U_EntDt, us.Site_Code, sm.Name as SiteName, sm.StateCode
+FROM Technoculture.dbo.UnitSale as us
+JOIN Technoculture.dbo.SiteMast as sm
+ON us.Site_Code = sm.Code
